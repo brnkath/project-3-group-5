ecc76a5f435b9a73eebc7805f9c7f750b091b9e9
NMcHale029 <noah.reetz@gmail.com>
2023-10-17 19:04:37 -0500

    initial Flask update

--- flask_api.py
+++ flask_api.py
@@ -12,7 +12,8 @@ from flask import Flask, render_template, jsonify, redirect, url_for
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///locations.sqlite")
+engine = create_engine("sqlite:///miami.sqlite",
+                       connect_args={'check_same_thread': False})
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -20,7 +21,8 @@ Base = automap_base()
 Base.prepare(autoload_with=engine)
 
 # Save reference to the table
-Passenger = Base.classes.locations
+Zip_income = Base.classes.zip_income
+Restaurants = Base.classes.restauants
 
 #################################################
 # Flask Setup
@@ -32,22 +34,27 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 
-
+# Can the routes have spaces?
 @app.route("/")
 def index():
     """List all available api routes."""
-    return f"Available Routes:<br/>" f"/api/v1.0/names<br/>" f"/api/v1.0/passengers"
-    return render_template("index.html", index=index)
+    return( 
+        f"Available Routes:<br/>" 
+        f"/api/v1.0/All Locations<br/>" 
+        f"/api/v1.0/Locations By Category<br/>"
+        f"/api/v1.0/Income Data<br/>"
+    return render_template("index.html", index=index))
 
 
-@app.route("/api/v1.0/names")
-def names():
+@app.route("/api/v1.0/All Locations")
+def locations():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
     """Return a list of all passenger names"""
-    # Query all passengers
-    results = session.query(Passenger.name).all()
+    # Query all restaurant names, addresses, and categories
+    results = session.query(Restaurants.Name, Restaurants.Address, Restaurants.Category)\
+        .order_by(Restaurants.name.asc()).all()
 
     session.close()
 
@@ -57,28 +64,53 @@ def names():
     return jsonify(all_names)
 
 
-@app.route("/api/v1.0/passengers")
-def passengers():
+@app.route("/api/v1.0/Locations By Category<br/>")
+def location_category():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
-    # Query all passengers
-    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+    # Query all restaurants
+    restaurants = session.query(Restaurants.Name, Restaurants.Address, Restaurants.Category)\
+        .filter(Restaurants.category == 'Restaurants')\
+        .order_by(Restaurants.name.asc()).all()
+
+    fast_food = session.query(Restaurants.Name, Restaurants.Address, Restaurants.Category)\
+        .filter(Restaurants.category == 'Fast Food')\
+        .order_by(Restaurants.name.asc()).all()
+
+    grocery_stores = session.query(Restaurants.Name, Restaurants.Address, Restaurants.Category)\
+        .filter(Restaurants.category == 'Grocery Store')\
+        .order_by(Restaurants.name.asc()).all()
 
     session.close()
 
-    # Create a dictionary from the row data and append to a list of all_passengers
-    all_passengers = []
-    for name, age, sex in results:
-        passenger_dict = {}
-        passenger_dict["name"] = name
-        passenger_dict["age"] = age
-        passenger_dict["sex"] = sex
-        all_passengers.append(passenger_dict)
+    # Select cetegory
 
-    return jsonify(all_passengers)
 
+    # Return the data from the appropriate category
+    if RESTAURANT:
+        return jsonify(restaurants)
+    
+    if FASTFOOD:
+        return jsonify(fast_food)
+    
+    if GROCERY:
+        return jsonify(grocery_stores)
+
+@app.route("/api/v1.0/Income Data<br/>")
+def zip_income():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query Income data in Miami zip codes
+    results = session.query(Zip_income.Zipcode, Zip_income.Median_income)
+    
+    session.close()
+
+    # Convert list of tuples into normal list
+    income = list(np.ravel(results))
+
+    return jsonify(income)
 
 if __name__ == "__main__":
     app.run(debug=True)
